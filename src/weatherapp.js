88a9f1007d3cc493e7be002feb550246c88b1a61
Saathviga9605 <saathviga9605@gmail.com>
2023-09-19 20:14:19 +0530

    Changed the JS such that celsius and fahrenheit temperature changes accordingly

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -44,8 +44,11 @@ function fetchWeather(city) {
       let humidityElement = document.querySelector("#humidity");
       let windElement = document.querySelector("#wind");
       let iconElement = document.querySelector("#icon");
+
+      celsiusTemperature= response.data.main.temp;
+
   
-      let temperature = Math.round(response.data.main.temp);
+      let temperature = Math.round(celsiusTemperature);
       let description = response.data.weather[0].description;
       let humidity = response.data.main.humidity;
       let windSpeed = Math.round(response.data.wind.speed * 3.6);
@@ -60,36 +63,63 @@ function fetchWeather(city) {
     });
   }
 
+  function showPosition(position) {
+    let apiurl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apikey}&units=metric`;
+  
+    axios
+      .get(apiurl)
+      .then(function (response) {
+        celsiusTemperature = response.data.main.temp; // Store the temperature in Celsius
+  
+        let temperatureElement = document.querySelector("#temperature");
+        let cityNameElement = document.querySelector(".city-name");
+        let temperature = Math.round(celsiusTemperature); 
+        
+        temperatureElement.innerHTML = `${temperature}`; 
+  
+        cityNameElement.innerHTML = response.data.name;
+        let humidityElement = document.querySelector("#humidity");
+        let windElement = document.querySelector("#wind");
+        let iconElement = document.querySelector("#icon");
+        humidityElement.innerHTML = response.data.main.humidity;
+        windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+        iconElement.setAttribute(
+          "src",
+          `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
+        iconElement.setAttribute("alt", response.data.weather[0].description);
+      })
+      .catch(function (error) {
+        console.error("Weather API Error:", error);
+      });
+  }
 
-function showPosition(position) {
-  let apiurl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apikey}&units=metric`;
-
-  axios
-    .get(apiurl)
-    .then(function (response) {
-      let temperatureElement = document.querySelector("#temperature");
-      let cityNameElement = document.querySelector(".city-name");
-      temperatureElement.innerHTML = response.data.main.temp;
-      cityNameElement.innerHTML = response.data.name;
-      let humidityElement = document.querySelector("#humidity");
-      let windElement = document.querySelector("#wind");
-      let iconElement = document.querySelector("#icon");
-      humidityElement.innerHTML = response.data.main.humidity;
-      windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
-      iconElement.setAttribute(
-        "src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-      );
-      iconElement.setAttribute("alt", response.data.weather[0].description);
-    
-    })
-    .catch(function (error) {
-      console.error("Weather API Error:", error);
-    });
-}
+  function changeintofahrenheit(event) {
+    event.preventDefault();
+    let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = `${Math.round(fahrenheitTemp)}`;
+  }
+  
+  function changeintocelsius(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = `${Math.round(celsiusTemperature)}`;
+  }
+  
 
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
+let celsiusTemperature=null;
+
 let button = document.querySelector("#current-location-btn");
 button.addEventListener("click", getCurrentPosition);
+
+let fahrenheit= document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", changeintofahrenheit);
+
+let celsius= document.querySelector("#celsius");
+celsius.addEventListener("click", changeintocelsius);
+
